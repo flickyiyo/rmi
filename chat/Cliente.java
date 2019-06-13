b33fc3edefb7565dc9d63da801fd391d6a16cbd9
Edgar Ivan Vazquez Camargo <edgar.i.vazquez@oracle.com>
2019-06-13 17:36:04 -0500

    JALP!

--- chat/Cliente.java
+++ chat/Cliente.java
@@ -4,29 +4,65 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class Cliente extends JFrame {
+    public static String ip;
     public static void main(String[] args) {
-        
+        ip = args[0];
+        new Cliente();
     }
 
-    JButton submit;
-    JLabel conver;
-    JTextField mensaje;
-    JTextField username;
-
+    JButton submitBtn;
+    JLabel converLbl;
+    JTextField mensajetxt;
+    JTextField usernametxt;
+    String username;
+    ChatRemoto chat;
     public Cliente() {
         this.setBounds(100, 100, 500, 500);
         this.setLayout(null);
 
-        submit = new JButton();
-        submit.setText("Enviar");
-        submit.setBounds(30, 400, 100, 50);
-        add(submit);
-
-        conver = new JLabel();
-        conver.setBounds(30, 10, 400, 500);
+        submitBtn = new JButton();
+        submitBtn.setText("Enviar");
+        submitBtn.setBounds(30, 400, 100, 50);
         
+        add(submitBtn);
 
+        converLbl = new JLabel();
+        converLbl.setBounds(30, 10, 400, 500);
+        add(converLbl);
+
+        usernametxt = new JTextField();
+        usernametxt.setBounds(150, 400, 100, 50);
+        usernametxt.setText("Edgar");
+        
+        add(usernametxt);
+        submitBtn.addActionListener(ev -> {
+            System.out.println(mensajetxt.getText());
+        });
+        usernametxt.addActionListener(ev -> {
+            username = usernametxt.getText();
+        });
+        mensajetxt = new JTextField();
+        mensajetxt.setBounds(30, 350, 300, 25);
+        add(mensajetxt);
+        try {
+            chat = (ChatRemoto)java.rmi.Naming.lookup("//" + ip + "PruebaRMI");
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void EnviarMensaje() {
+
+    }
+
+    public void getMensajes() {
+        ArrayList<Mensaje> mensajes = chat.getMensajes();
+        String lbltext = "";
+        for (Mensaje mensaje: mensajes) {
+            lbltext.concat(mensaje.username+": " +mensaje.mensaje+"\n");
+        }
+        this.converLbl.setText(lbltext);
     }
 }
